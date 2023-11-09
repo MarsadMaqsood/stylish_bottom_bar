2119a58a96761a8c0852be29ffab8cc52465c088
MarsadMaqsood <marsadmaqsood149@gmail.com>
2023-11-09 15:18:00 +0500

    added `bar_items` export

--- lib/stylish_bottom_bar.dart
+++ lib/stylish_bottom_bar.dart
@@ -3,3 +3,4 @@ library stylish_bottom_bar;
 export 'package:stylish_bottom_bar/helpers/enums.dart' hide MovingStatus;
 export 'package:stylish_bottom_bar/src/bottom_bar.dart';
 export 'package:stylish_bottom_bar/model/options.dart';
+export 'package:stylish_bottom_bar/model/bar_items.dart';
