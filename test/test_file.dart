a462b16262280805c3c440bff1b587737f16d759
MarsadMaqsood <marsadmaqsood149@gmail.com>
2023-11-09 18:02:46 +0500

    updated

--- test/test_file.dart
+++ test/test_file.dart
@@ -1,7 +1,116 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:stylish_bottom_bar/model/bar_items.dart';
 import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
+
+class DotBarExample extends StatefulWidget {
+  const DotBarExample({super.key});
+
+  @override
+  State<DotBarExample> createState() => _DotBarExampleState();
+}
+
+class _DotBarExampleState extends State<DotBarExample> {
+  dynamic selected;
+  var heart = false;
+  PageController controller = PageController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true, //to make floating action button notch transparent
+
+      //to avoid the floating action button overlapping behavior,
+      // when a soft keyboard is displayed
+      // resizeToAvoidBottomInset: false,
+
+      bottomNavigationBar: StylishBottomBar(
+        option: DotBarOptions(),
+        items: [
+          BottomBarItem(
+            icon: const Icon(
+              Icons.house_outlined,
+            ),
+            selectedIcon: const Icon(Icons.house_rounded),
+            // selectedColor: Colors.teal,
+            backgroundColor: Colors.teal,
+            title: const Text('Home'),
+            badge: const Text('9+'),
+            showBadge: true,
+            badgeColor: Colors.purple,
+            badgePadding: const EdgeInsets.only(left: 4, right: 4),
+          ),
+          BottomBarItem(
+            icon: const Icon(Icons.star_border_rounded),
+            selectedIcon: const Icon(Icons.star_rounded),
+            selectedColor: Colors.red,
+            // unSelectedColor: Colors.purple,
+            // backgroundColor: Colors.orange,
+            title: const Text('Star'),
+          ),
+          BottomBarItem(
+              icon: const Icon(
+                Icons.style_outlined,
+              ),
+              selectedIcon: const Icon(
+                Icons.style,
+              ),
+              backgroundColor: Colors.amber,
+              selectedColor: Colors.deepOrangeAccent,
+              title: const Text('Style')),
+          BottomBarItem(
+              icon: const Icon(
+                Icons.person_outline,
+              ),
+              selectedIcon: const Icon(
+                Icons.person,
+              ),
+              backgroundColor: Colors.purpleAccent,
+              selectedColor: Colors.deepPurple,
+              title: const Text('Profile')),
+        ],
+        hasNotch: true,
+        fabLocation: StylishBarFabLocation.center,
+        currentIndex: selected ?? 0,
+        onTap: (index) {
+          controller.jumpToPage(index);
+          setState(() {
+            selected = index;
+          });
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            heart = !heart;
+          });
+        },
+        backgroundColor: Colors.white,
+        child: Icon(
+          heart ? CupertinoIcons.heart_fill : CupertinoIcons.heart,
+          color: Colors.red,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: SafeArea(
+        child: PageView(
+          controller: controller,
+          children: const [
+            Center(child: Text('Home')),
+            Center(child: Text('Star')),
+            Center(child: Text('Style')),
+            Center(child: Text('Profile')),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class AnimatedBarExample extends StatefulWidget {
   const AnimatedBarExample({super.key});
